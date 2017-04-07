c805fceee8609ae30b02de2f909fd08e8a994842
Daniel Rey Lopez <daniel.rey.lopez@automattic.com>
2017-04-07 11:46:50 +0100

    [MAIN COMMIT] Change the build system to use NPM scripts instead of Makefile, making it fully Windows-compatible

--- webpack.config.js
+++ webpack.config.js
@@ -42,7 +42,7 @@ const webpackConfig = {
 		noParse: /[\/\\]node_modules[\/\\]localforage[\/\\]dist[\/\\]localforage\.js$/,
 		loaders: [
 			{
-				test: /extensions\/index/,
+				test: /extensions[\/\\]index/,
 				exclude: 'node_modules',
 				loader: path.join( __dirname, 'server', 'bundler', 'extensions-loader' )
 			},
@@ -64,7 +64,7 @@ const webpackConfig = {
 				loader: 'exports?window.tinymce',
 			},
 			{
-				include: /node_modules\/tinymce/,
+				include: /node_modules[\/\\]tinymce/,
 				loader: 'imports?this=>window',
 			}
 		]
